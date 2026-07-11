e47ff68802154d68026fafba0f019fce7bc34064
Isadora Alexandre <isadoraalexandre489@gmail.com>
2026-07-10 21:15:37 -0300

    atualiza fct_intervalo_ocorrencia e fct_ranking_beat referenciar as dims corretas

--- dbt/crimes_chicago/models/marts/fct_intervalo_ocorrencia.sql
+++ dbt/crimes_chicago/models/marts/fct_intervalo_ocorrencia.sql
@@ -10,10 +10,12 @@ with eventos as (
         fc.sk_local,
         fc.sk_tipo_crime,
         fc.date,
+
         lag(fc.date) over (
-            partition by fc.sk_local, fc.sk_tipo_crime, fc.date::date
+            partition by fc.sk_local, fc.sk_tipo_crime
             order by fc.date
         ) as data_anterior
+
     from {{ ref('fct_crime') }} fc
 
 ),
@@ -23,9 +25,12 @@ com_intervalo as (
     select
         sk_local,
         sk_tipo_crime,
-        date::date                                              as data_ocorrencia,
+        date::date as data_ocorrencia,
         date,
-        extract(epoch from (date - data_anterior)) / 60.0 as intervalo_minutos
+
+        extract(epoch from (date - data_anterior)) / 3600.0
+            as lag_ocorrencia
+
     from eventos
     where data_anterior is not null
 
@@ -37,22 +42,38 @@ por_dia as (
         sk_local,
         sk_tipo_crime,
         data_ocorrencia,
-        avg(intervalo_minutos)                                          as media_intervalo,
-        -- lag_ocorrencia = intervalo referente à última ocorrência do dia
-        (array_agg(intervalo_minutos order by date desc))[1]     as lag_ocorrencia
+
+        avg(lag_ocorrencia) as media_intervalo,
+
+        (
+            array_agg(lag_ocorrencia order by date desc)
+        )[1] as lag_ocorrencia
+
     from com_intervalo
-    group by 1, 2, 3
+
+    group by
+        sk_local,
+        sk_tipo_crime,
+        data_ocorrencia
 
 )
 
 select
-    {{ dbt_utils.generate_surrogate_key(['sk_local', 'sk_tipo_crime', 'data_ocorrencia']) }} as id,
+
+    {{
+        dbt_utils.generate_surrogate_key(
+            ['sk_local', 'sk_tipo_crime', 'data_ocorrencia']
+        )
+    }} as id,
+
     dt.sk_tempo,
     p.sk_local,
     p.sk_tipo_crime,
     p.lag_ocorrencia,
     p.media_intervalo
+
 from por_dia p
+
 inner join {{ ref('dim_tempo') }} dt
     on dt.data_completa = p.data_ocorrencia
-    and dt.hora = 0
+   and dt.hora = 0
--- dbt/crimes_chicago/models/marts/fct_ranking_beat.sql
+++ dbt/crimes_chicago/models/marts/fct_ranking_beat.sql
@@ -10,16 +10,20 @@ with base as (
         dt.ano,
         dt.mes,
         sum(fc.quant_crime) as qtd_crimes_mes
+
     from {{ ref('fct_crime') }} fc
     inner join {{ ref('dim_tempo') }} dt
         on dt.sk_tempo = fc.sk_tempo
-    group by 1, 2, 3
+    group by
+        fc.sk_local,
+        dt.ano,
+        dt.mes
 ),
 
 ranqueado as (
     select
         *,
-        rank() over (
+        dense_rank() over (
             partition by ano, mes
             order by qtd_crimes_mes desc
         ) as ranking_posicao
@@ -34,18 +38,30 @@ com_variacao as (
             order by r.ano, r.mes
         ) as ranking_posicao_mes_anterior
     from ranqueado r
+),
+
+ultimo_dia_mes as (
+    select
+        ano,
+        mes,
+        max(data_completa) as data_fim_mes
+    from {{ ref('dim_tempo') }}
+    group by
+        ano,
+        mes
 )
 
 select
-    {{ dbt_utils.generate_surrogate_key(['c.sk_local', 'c.ano', 'c.mes']) }} as id,
-    dt_mes.sk_tempo,
+    {{ dbt_utils.generate_surrogate_key(['c.sk_local', 'c.ano', 'c.mes'])}} as id,
+    dt.sk_tempo,
     c.sk_local,
     c.qtd_crimes_mes,
     c.ranking_posicao,
     (c.ranking_posicao_mes_anterior - c.ranking_posicao) as variacao_ranking
 from com_variacao c
-inner join {{ ref('dim_tempo') }} dt_mes
-    on dt_mes.ano = c.ano
-    and dt_mes.mes = c.mes
-    and dt_mes.dia = 1
-    and dt_mes.hora = 0
+inner join ultimo_dia_mes u
+    on u.ano = c.ano
+   and u.mes = c.mes
+inner join {{ ref('dim_tempo') }} dt
+    on dt.data_completa = u.data_fim_mes
+   and dt.hora = 0
